0a431169c2e8244bf3e90b2d694665f9113701a6
Sarah Wade <sswade@mtu.edu>
2023-09-03 15:49:46 -0600

    Added the ability to parse the match description into map/side picks

--- soup_test.py
+++ soup_test.py
@@ -1,7 +1,11 @@
 from vlrDB import vlrDB
+import re
 
 
 url = "https://www.vlr.gg/248273/evil-geniuses-vs-edward-gaming-valorant-champions-2023-playoffs-ubqf/"
+url = "https://www.vlr.gg/247077/paper-rex-vs-edward-gaming-valorant-champions-2023-group-stage-winners-a"
+url = "https://www.vlr.gg/232735/kr-esports-vs-leviat-n-champions-tour-2023-americas-last-chance-qualifier-gf"
+url = 'https://www.vlr.gg/167393/loud-vs-fnatic-champions-tour-2023-lock-in-s-o-paulo-gf'
 file = open("vlrtest.html","w")
 
 db = vlrDB()
@@ -13,4 +17,6 @@ print(str(EG_EDG))
 
 file.write(str(EG_EDG))
 file.close()
+
+tst = "EDG ban Lotus; PRX ban Haven; EDG pick Fracture; PRX pick Pearl; EDG ban Split; PRX ban Ascent; Bind remains"
 
--- vlrDB.py
+++ vlrDB.py
@@ -4,30 +4,66 @@ import re
 import requests
 
 
+### --------- TODO ---------
+# Make grabbing team name vs team short name automatic (really only matters for the map veto. right now i can only do champs teams)
+
+na_short = ["100T", "C9", "EG", "NRG", "SEN"]
+na_teams = ["100 Thieves", "Cloud9", "Evil Geniuses", "NRG Esports", "Sentinels"]
+br_short = ["FUR", "LOUD", "MIBR"]
+br_teams = ["Furia", "LOUD", "MIBR"]
+lt_short = ["KRÜ", "LEV"]
+lt_teams = ["KRÜ Esports", "Leviatán"]
+fr_short = ["KC","VIT"]
+fr_teams = ["Karmine Corp","Team Vitality"]
+es_short = ["GIA","KOI","TH"]
+es_teams = ["Giants Gaming", "KOI", "Team Heretics"]
+tk_short = ["BBL","FUT"]
+tk_teams = ["BBL Esports","FUT Esports"]
+nl_short = ["TL"]
+nl_teams = ["Team Liquid"]
+uk_short = ["FNC"]
+uk_teams = ["FNATIC"]
+ua_short = ["NAVI"]
+ua_teams = ["Natus Vincere"]
+kr_short = ["DRX","GEN","T1"]
+kr_teams = ["DRX","Gen.G","T1"]
+jp_short = ["DFM","ZETA"]
+jp_teams = ["DetonatioN FocusMe","ZETA DIVISION"]
+sg_short = ["PRX"]
+sg_teams = ["Paper Rex"]
+ph_short = ["TS"]
+ph_teams = ["Team Secret"]
+th_short = ["TLN"]
+th_teams = ["Talon Esports"]
+id_short = ["RRQ"]
+id_teams = ["Rex Regum Qeon "]
+in_short = ["GE"]
+in_teams = ["Global Esports"]
+cn_short = ["EDG","FPX","BLG"]
+cn_teams = ["EDward Gaming","FunPlus Phoenix","Bilibili Gaming"]
+team_names = dict(zip(na_short,na_teams))
+team_names.update(dict(zip(br_short,br_teams)))
+team_names.update(dict(zip(lt_short,lt_teams)))
+team_names.update(dict(zip(fr_short,fr_teams)))
+team_names.update(dict(zip(es_short,es_teams)))
+team_names.update(dict(zip(tk_short,tk_teams)))
+team_names.update(dict(zip(nl_short,nl_teams)))
+team_names.update(dict(zip(uk_short,uk_teams)))
+team_names.update(dict(zip(ua_short,ua_teams)))
+team_names.update(dict(zip(kr_short,kr_teams)))
+team_names.update(dict(zip(jp_short,jp_teams)))
+team_names.update(dict(zip(sg_short,sg_teams)))
+team_names.update(dict(zip(ph_short,ph_teams)))
+team_names.update(dict(zip(th_short,th_teams)))
+team_names.update(dict(zip(id_short,id_teams)))
+team_names.update(dict(zip(in_short,in_teams)))
+team_names.update(dict(zip(cn_short,cn_teams)))
+
+teams = na_teams+br_teams+lt_teams+fr_teams+es_teams+tk_teams+uk_teams+ua_teams+nl_teams
+short = na_short+br_short+lt_short+fr_short+es_short+tk_short+uk_short+ua_short+nl_short
 class vlrDB: 
 	"""Basic Structure to operate on Valorant data pulled from VLR"""
-	na_short = ["100T", "C9", "EG", "NRG", "SEN"]
-	na_teams = ["100 Thieves", "Cloud 9", "Evil Geniuses", "NRG", "Sentinels"]
-	br_short = ["FUR", "LOUD", "MIBR"]
-	br_teams = ["Furia", "LOUD", "Made In Brazil"]
-	lt_short = ["KRU", "LEV"]
-	lt_teams = ["KRU", "Leviatan"]
 
-	fr_short = ["KC","VIT"]
-	fr_teams = ["Karmine Corp","Team Vitality"]
-	es_short = ["GIA","KOI","TH"]
-	es_teams = ["Giants", "KOI", "Team Heratics"]
-	tk_short = ["BBL","FUT"]
-	tk_teams = ["BBL","FUT"]
-	nl_short = ["TL"]
-	nl_teams = ["Team Liquid"]
-	uk_short = ["FNC"]
-	uk_teams = ["FNATIC"]
-	ua_short = ["NAVI"]
-	ua_teams = ["Natus Vincere"]
-
-	teams = na_teams+br_teams+lt_teams+fr_teams+es_teams+tk_teams+uk_teams+ua_teams+nl_teams
-	short = na_short+br_short+lt_short+fr_short+es_short+tk_short+uk_short+ua_short+nl_short
 
 	class Match: 
 		length = 3
@@ -39,29 +75,31 @@ class vlrDB:
 		match_desc = ""
 		map_veto = ""
 
+		map_template = {
+			"map" 			: ["_"],
+			"winner" 		: ["_"],
+			"loser"			: ["_"],
+			"map_pick" 	: ["_"],
+			"side_pick" : ["_"],
+			"team1" 		: ["_"],
+			"team1_score" : [0],
+			"team2" 		: ["_"],
+			"team2_score" : [0],
+			"t1_team" 	: ["_"],
+			"t1_rounds"	: [0],
+			"ct1_team" 	: ["_"],
+			"ct1_rounds" : [0],
+			"t2_team" 	: ["_"],
+			"t2_rounds" : [0],
+			"ct2_team"	: ["_"],
+			"ct2_rounds" : [0],
+			"team1_ot" 	: [0],
+			"team2_ot" 	: [0]
+			}
+
 		def __init__(self):
-			map_template = {
-				"map" : ["_","_","_"],
-				"winner" : ["_","_","_"],
-				"loser"  : ["_","_","_"],
-				"map_pick" : ["_","_","_"],
-				"side_pick" : ["_","_","_"],
-				"team1" : ["_","_","_"],
-				"team1_score" : [0,0,0],
-				"team2" : ["_","_","_"],
-				"team2_score" : [0,0,0],
-				"attack1_team" : ["_","_","_"],
-				"attack1_rounds" : [0,0,0],
-				"defense1_team" : ["_","_","_"],
-				"defense1_rounds" : [0,0,0],
-				"attack2_team" : ["_","_","_"],
-				"attack2_rounds" : [0,0,0],
-				"defense2_team": ["_","_","_"],
-				"defense2_rounds" : [0,0,0],
-				"team1_ot" :  [0,0,0],
-				"team2_ot" : [0,0,0]
-				}
-			self.maps = pd.DataFrame(map_template)
+			cols = ['map','winner','loser','map_pick','side_pick','team1','team1_score','team2','team2_score','t1_team','t1_rounds','ct1_team','ct1_rounds','t2_team','t2_rounds','ct2_team','ct2_rounds','team1_ot','team2_ot']
+			self.maps = pd.DataFrame(columns=cols)
 			
 		def __str__(self):
 			ret = f'{self.event_series} - {self.match_desc} \n'
@@ -69,88 +107,115 @@ class vlrDB:
 			ret += str(self.maps)
 			return ret
 
-		def parse_map(self, map_number, map_stats):
+		def parse_map(self, map_stats):
+			info = {}
 			map = map_stats(attrs={'class':'map'})
 			map = re.search("^([\S]+)",map[0].text.strip())
 			map = map.group()
-			self.maps.loc[self.maps.index[map_number], 'map'] = map
+			info['map'] = map
 	
 			teams_list = map_stats(attrs={'class':'team'})
 			# ----- Team 1 -----
 			team1_line = teams_list[0] # Tag object that has all the data for Team1
 			# Team1 Name
 			team1_name = team1_line(attrs={'class':'team-name'})[0] # Tag object for the team name
-			self.maps.loc[self.maps.index[map_number], 'team1'] = team1_name.text.strip() # Populate Dictionary
+			info['team1'] = team1_name.text.strip() # Populate Dictionary
 			# Team1 Score
 			team1_score = team1_line(attrs={'class':'score'})
 			t1_score = team1_score[0]
 			if 'mod-win' in t1_score.attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'winner'] = team1_name.text.strip()
+				info['winner'] = team1_name.text.strip()
 			else: 
-				self.maps.loc[self.maps.index[map_number], 'loser'] = team1_name.text.strip()
-			self.maps.loc[self.maps.index[map_number], 'team1_score'] = int(t1_score.text.strip())
+				info['loser'] = team1_name.text.strip()
+			info['team1_score'] = int(t1_score.text.strip())
 			# Assign Team1 to respective sides
 			team1_score_list = team1_line('span')
 			if 'mod-ct' in team1_score_list[0].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'defense1_team'] = team1_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'defense1_rounds'] = team1_score_list[0].text.strip()
+				info['ct1_team'] = team1_name.text.strip()
+				info['ct1_rounds'] = team1_score_list[0].text.strip()
 			elif 'mod-t' in team1_score_list[0].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'attack1_team'] = team1_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'attack1_rounds'] = team1_score_list[0].text.strip()
+				info['t1_team'] = team1_name.text.strip()
+				info['t1_rounds'] = team1_score_list[0].text.strip()
 			if 'mod-ct' in team1_score_list[1].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'defense2_team'] = team1_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'defense2_rounds'] = team1_score_list[1].text.strip()
+				info['ct2_team'] = team1_name.text.strip()
+				info['ct2_rounds'] = team1_score_list[1].text.strip()
 			elif 'mod-t' in team1_score_list[1].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'attack2_team'] = team1_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'attack2_rounds'] = team1_score_list[1].text.strip()
+				info['t2_team'] = team1_name.text.strip()
+				info['t2_rounds'] = team1_score_list[1].text.strip()
 			if len(team1_line(attrs={'class':'mod-ot'})) > 0:
-				self.maps.loc[self.maps.index[map_number], 'team1_ot'] = int(team1_line(attrs={'class':'mod-ot'})[0].text.strip())
+				info['team1_ot'] = int(team1_line(attrs={'class':'mod-ot'})[0].text.strip())
+			else:
+				info['team1_ot'] = 0
 	
 			# ----- Team 2 -----
 			team2_line = teams_list[1]
+			# Team2 Name
 			team2_name = team2_line(attrs={'class':'team-name'})[0] # Tag object for the team name
-			self.maps.loc[self.maps.index[map_number], 'team2'] = team2_name.text.strip()
-			#team2_it = team2_line.contents
+			info['team2'] = team2_name.text.strip()
+			# Team2 Score
 			team2_score = team2_line(attrs={'class':'score'})
 			t2_score = team2_score[0]
 			if 'mod-win' in t2_score.attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'winner'] = team2_name.text.strip()
+				info['winner'] = team2_name.text.strip()
 			else: 
-				self.maps.loc[self.maps.index[map_number], 'loser'] = team2_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'team2_score'] = int(t2_score.text.strip())
+				info['loser'] = team2_name.text.strip()
+			info['team2_score'] = int(t2_score.text.strip())
 			# Assign Team2 to respective sides
 			team2_score_list = team2_line('span')
 			if 'mod-ct' in team2_score_list[0].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'defense1_team'] = team2_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'defense1_rounds'] = team2_score_list[0].text.strip()
+				info['ct1_team'] = team2_name.text.strip()
+				info['ct1_rounds'] = team2_score_list[0].text.strip()
 			elif 'mod-t' in team2_score_list[0].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'attack1_team'] = team2_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'attack1_rounds'] = team2_score_list[0].text.strip()
+				info['t1_team'] = team2_name.text.strip()
+				info['t1_rounds'] = team2_score_list[0].text.strip()
 			if 'mod-ct' in team2_score_list[1].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'defense2_team'] = team2_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'defense2_rounds'] = team2_score_list[1].text.strip()
+				info['ct2_team'] = team2_name.text.strip()
+				info['ct2_rounds'] = team2_score_list[1].text.strip()
 			elif 'mod-t' in team2_score_list[1].attrs['class']:
-				self.maps.loc[self.maps.index[map_number], 'attack2_team'] = team2_name.text.strip()
-				self.maps.loc[self.maps.index[map_number], 'attack2_rounds'] = team2_score_list[1].text.strip()
+				info['t2_team'] = team2_name.text.strip()
+				info['t2_rounds'] = team2_score_list[1].text.strip()
 			if len(team2_line(attrs={'class':'mod-ot'})) > 0:
-				self.maps.loc[self.maps.index[map_number], 'team2_ot'] = int(team2_line(attrs={'class':'mod-ot'})[0].text.strip())
+				info['team2_ot'] = int(team2_line(attrs={'class':'mod-ot'})[0].text.strip())
+			else: 
+				info['team2_ot'] = 0
+
+			df = pd.DataFrame(info,index=[0])
+			return df
 
 		def parse_match(self, url):
 			page = requests.get(url)
 			soup = BeautifulSoup(page.text, 'html.parser')
 			maps = soup(attrs={"class":"vm-stats-game"}) # contains m+1 items where m is the number of maps available (3 in BO3, 5 in BO5)
-			self.parse_map(0, maps[0]) # index 1 is "all map stats"
-			self.parse_map(1, maps[2])
-			self.parse_map(2, maps[3])
-			self.winner = self.maps['winner'].mode()
-			self.team1 = self.maps['team1'].mode()
-			self.team2 = self.maps['team2'].mode()
+			for map_line in maps:
+				if map_line.attrs['data-game-id']=='all':
+					pass
+				else:
+					self.maps = pd.concat([self.maps,self.parse_map(map_line)],ignore_index=True)
+			self.winner = self.maps['winner'].mode()[0]
+			self.team1 = self.maps['team1'].mode()[0]
+			self.team2 = self.maps['team2'].mode()[0]
 			self.event_series = soup(attrs={"class":"match-header-event-series"})[0].previous_sibling.previous_sibling.text.strip()
 			self.event_series = re.sub("\s+"," ",self.event_series)
 			self.match_desc = soup(attrs={"class":"match-header-event-series"})[0].text.strip()
 			self.match_desc = re.sub("\s+"," ",self.match_desc)
 			self.map_veto = soup(attrs={"class":"match-header-note"})[0].text.strip()
 			self.map_veto = re.sub("\s+"," ",self.map_veto)
+			split = self.map_veto.split('; ')
+			picks = []
+			for entry in split:
+				if 'pick' in entry:
+					picks.append(entry)
+			for x in range(len(self.maps.index)):
+				if x == len(picks):
+					self.maps.at[x,"map_pick"] = 'remains'
+					self.maps.at[x,"side_pick"] = self.maps.at[0,"map_pick"]
+				else:
+					team = team_names[picks[x].split()[0]]
+					self.maps.at[x,"map_pick"] = team
+					if team == self.team1:
+						self.maps.at[x,"side_pick"] = self.team2
+					elif team == self.team2:
+						self.maps.at[x,"side_pick"] = self.team1
 
 	class Team:
 		name = ""
@@ -165,30 +230,30 @@ class vlrDB:
 			self.region = region
 			self.subregion = subregion
 
-	def __init__(self):
-		AMER = []
-		for name, short in zip(self.na_teams,self.na_short):
-			AMER.append(self.Team(name, short, "AMER","NA")) 
-		for name, short in zip(self.br_teams,self.br_short):
-			AMER.append(self.Team(name, short, "AMER","BR")) 
-		for name, short in zip(self.lt_teams,self.lt_short):
-			AMER.append(self.Team(name, short, "AMER","LT"))
-
-		EMEA =[]
-		for name, short in zip(self.fr_teams,self.fr_short):
-			EMEA.append(self.Team(name, short, "EMEA","FR")) 
-		for name, short in zip(self.es_teams,self.es_short):
-			EMEA.append(self.Team(name, short, "EMEA","ES")) 
-		for name, short in zip(self.tk_teams,self.tk_short):
-			EMEA.append(self.Team(name, short, "EMEA","TK")) 
-		for name, short in zip(self.nl_teams,self.nl_short):
-			EMEA.append(self.Team(name, short, "EMEA","NL")) 
-		for name, short in zip(self.uk_teams,self.uk_short):
-			EMEA.append(self.Team(name, short, "EMEA","UK")) 
-		for name, short in zip(self.ua_teams,self.ua_short):
-			EMEA.append(self.Team(name, short, "EMEA","UA")) 
-
-		#do apac...
-
-		regions = [AMER,"APAC",EMEA]
+	#def __init__(self):
+	#	AMER = []
+	#	for name, short in zip(self.na_teams,self.na_short):
+	#		AMER.append(self.Team(name, short, "AMER","NA")) 
+	#	for name, short in zip(self.br_teams,self.br_short):
+	#		AMER.append(self.Team(name, short, "AMER","BR")) 
+	#	for name, short in zip(self.lt_teams,self.lt_short):
+	#		AMER.append(self.Team(name, short, "AMER","LT"))
+#
+	#	EMEA =[]
+	#	for name, short in zip(self.fr_teams,self.fr_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","FR")) 
+	#	for name, short in zip(self.es_teams,self.es_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","ES")) 
+	#	for name, short in zip(self.tk_teams,self.tk_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","TK")) 
+	#	for name, short in zip(self.nl_teams,self.nl_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","NL")) 
+	#	for name, short in zip(self.uk_teams,self.uk_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","UK")) 
+	#	for name, short in zip(self.ua_teams,self.ua_short):
+	#		EMEA.append(self.Team(name, short, "EMEA","UA")) 
+#
+	#	#do apac...
+#
+	#	regions = [AMER,"APAC",EMEA]
 	
